453d69a5f5cdc890ee2d47018b167322e79ed775
muhamadzade <muhamadzade@gmail.com>
2023-07-16 19:35:40 +0330

    phase4

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     p = Post.objects.filter(published_date__lte=timezone.now()
@@ -8,3 +8,8 @@ def post_list(request):
     # posts = Post.objects.all().order_by('published_date')
     print(p)
     return render(request, 'blog/post_list.html', {'posts': p})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
